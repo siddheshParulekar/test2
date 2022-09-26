a4c2e297a8044c676c89262ac191ab8e863c5fb2
Umesh Maurya <109727218+mauryaumesh@users.noreply.github.com>
2022-09-26 16:55:03 +0530

    Update test2.java
    second commit

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -5,5 +5,6 @@ public class test2 {
 	public static void main (String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Hello java developer");
+		System.out.println("Hello World");
 	}
 }
